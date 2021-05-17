82f5c118e4a1a63cb9028f347486b9115d35c48f
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-05-17 13:43:28 -0700

    feat: add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig,task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import '@typechain/hardhat'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,11 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 
  const config: HardhatUserConfig = {
   solidity: "0.7.3",
+  typechain: {
+    outDir: 'types',
+    target: 'ethers-v5',
+  },
+
 };
 
 export default config;
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,9 +1,9 @@
-import { ethers } from "hardhat";
+import * as hre from "hardhat";
 import { expect } from 'chai';
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
+    const Greeter = await hre.ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
 
     await greeter.deployed();
