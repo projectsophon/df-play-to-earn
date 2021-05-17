41d56fbd0357c5a530c86ef93c2b1e0d6a4478bf
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-05-17 13:37:00 -0700

    feat: switch to typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
-require("@nomiclabs/hardhat-waffle");
+import { HardhatUserConfig,task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -13,10 +14,9 @@ task("accounts", "Prints the list of accounts", async () => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+ const config: HardhatUserConfig = {
   solidity: "0.7.3",
 };
+
+export default config;
 
